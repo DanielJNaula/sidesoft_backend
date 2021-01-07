1f80cbd185a684e5b1c49d5d7cdd548aee3deaea
= <=>
2021-01-06 20:49:38 -0500

    Inicializacion del proyecto con migraciones y relaciones de modelos

---
+++ app/Models/ImagenProducto.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Image;
+
+class ImagenProducto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'path',
+        'producto_id'
+    ];
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
+
+    public static function guardarImagen($request)
+    {
+        $file          = $request->file('imagen');
+        $nombre_imagen = time() . '-' . strtr($request->nombre, " ", "_") . '.' . $file->getClientOriginalExtension();
+        $path          = '/sidesoft/productos/' . $nombre_imagen;
+        $imagen        = Image::make($file->getRealPath())->save($path);
+        return $path;
+    }
+}
